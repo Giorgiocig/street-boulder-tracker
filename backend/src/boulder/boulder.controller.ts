399941e871c2162e5e759cc63856014e151ac01c
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-06-18 15:32:49 +0200

    Implement boulder controller (backend)

--- backend/src/boulder/boulder.controller.ts
+++ backend/src/boulder/boulder.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { BoulderService } from './boulder.service';
+import { BoulderDto } from './dto';
+import { IBoulder } from 'src/utilities/interfaces';
 
 @Controller('boulders')
 export class BoulderController {
-  constructor(private boulderService: BoulderService) {}
+  constructor(private readonly boulderService: BoulderService) {}
 
   @Post()
-  async insertBoulder(@Body() data: any) {
-    return this.boulderService.insertBoulder(data);
+  async insertBoulder(@Body() dto: BoulderDto): Promise<any> {
+    return await this.boulderService.insertBoulder(dto);
   }
 }
