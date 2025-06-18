54aa438a680e6446a35df71c658704e2383a3fa3
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-06-18 15:32:21 +0200

    Implement boulder dto (backend)

---
+++ backend/src/boulder/dto/boulder.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsEnum,
+  IsISO8601,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Difficulty } from 'src/utilities/enums/enums';
+
+export class BoulderDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsEnum(Difficulty)
+  difficulty: Difficulty;
+
+  @IsNotEmpty()
+  @IsNumber()
+  latitude: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  longitude: number;
+
+  @IsNotEmpty()
+  @IsISO8601()
+  createdAt: string;
+
+  @IsOptional()
+  @IsNumber()
+  userId?: number;
+}
