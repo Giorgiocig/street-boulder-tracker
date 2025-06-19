59047c01d4ca9722cf1b4b478bc26bd303266a72
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-06-19 11:15:02 +0200

    Add boulderServiceFixtures

---
+++ backend/src/boulder/fixture/boulderServiceFixtures.ts
@@ -0,0 +1,14 @@
+import { Difficulty } from 'src/utilities/enums/enums';
+import { BoulderDto } from '../dto';
+
+export const createBaseDto = (userId?: number): BoulderDto => {
+  const baseDto: BoulderDto = {
+    name: 'Test Boulder',
+    description: 'Nice boulder',
+    difficulty: Difficulty.facile,
+    latitude: 45.123,
+    longitude: 7.123,
+    createdAt: '2025-06-18T14:30:00.000Z',
+  };
+  return !userId ? baseDto : { ...baseDto, userId: userId };
+};
