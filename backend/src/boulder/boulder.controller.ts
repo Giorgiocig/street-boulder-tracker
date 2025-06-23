28107f5b38c62073e0263cc735bb977f74c99c21
Giorgiocig <giorgiocignitti@gmail.com>
2025-06-23 11:39:33 +0200

    Add /insert to boulder route (backend)

--- backend/src/boulder/boulder.controller.ts
+++ backend/src/boulder/boulder.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { BoulderService } from './boulder.service';
 import { BoulderDto } from './dto';
 
-@Controller('boulders')
+@Controller('v1/boulders')
 export class BoulderController {
   constructor(private readonly boulderService: BoulderService) {}
 
-  @Post()
+  @Post('/insert')
   async insertBoulder(@Body() dto: BoulderDto): Promise<any> {
     return await this.boulderService.insertBoulder(dto);
   }
