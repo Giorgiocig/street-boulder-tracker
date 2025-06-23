c3f4cddf8e1a7927d58f42b3aff2009f7cab59be
Giorgiocig <giorgiocignitti@gmail.com>
2025-06-23 11:49:03 +0200

    Add fixtures for e2e test (backend)

---
+++ backend/test/fixture/app.e2eFixture.ts
@@ -0,0 +1,13 @@
+import { BoulderDto } from 'src/boulder/dto';
+import { Difficulty } from 'src/utilities/enums/enums';
+
+export * from './app.e2eFixture';
+
+export const boulderDtoFixture: BoulderDto = {
+  name: 'Test Boulder',
+  description: 'Nice boulder',
+  difficulty: Difficulty.facile,
+  latitude: 45.123,
+  longitude: 7.123,
+  createdAt: '2025-06-18T14:30:00.000Z',
+};
