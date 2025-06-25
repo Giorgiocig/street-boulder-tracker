32a0a931f4928778978d07c74046c7cd06d19b4d
Giorgiocig <giorgiocignitti@gmail.com>
2025-06-25 14:19:19 +0200

    Embeed FormFieldContainer into form field  (frontend)

--- frontend/src/components/layout/BoulderForm.tsx
+++ frontend/src/components/layout/BoulderForm.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Button, TextField, Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { useState } from "react";
 import PublishIcon from "@mui/icons-material/Publish";
-import SelectForm from "../form/SelectForm";
-import { DIFFICULTY_SELECT_FIELD } from "../../utilities/constants";
+import { DIFFICULTY_SELECT_MENU_ITEMS } from "../../utilities/constants";
 import type { BoulderFormData } from "../../utilities/interfaces";
+import FormFieldsContainer from "./FormFieldsContainer";
 
 export default function BoulderForm() {
   const [formData, setFormData] = useState<BoulderFormData>({
@@ -30,41 +30,46 @@ export default function BoulderForm() {
   return (
     <form onSubmit={handleSubmit}>
       <Typography>helllo</Typography>
-      <TextField
-        id="textfield-name"
-        label="nome"
-        name="name"
-        variant="outlined"
-        onChange={handleChange}
-        value={formData.name}
-      />
-      <TextField
-        id="textfield-description"
-        label="descrizione"
-        variant="outlined"
-        name="description"
-        value={formData.description}
-        onChange={handleChange}
-      />
-      <SelectForm
-        setFormData={setFormData}
-        menuItems={DIFFICULTY_SELECT_FIELD}
-      />
-      <TextField
-        id="textfield-lat"
-        label="lat"
-        variant="outlined"
-        name="lat"
-        value={formData.lat}
-        onChange={handleChange}
-      />
-      <TextField
-        id="textfield-long"
-        label="long"
-        variant="outlined"
-        name="long"
-        value={formData.long}
-        onChange={handleChange}
+      <FormFieldsContainer
+        fields={[
+          {
+            id: "textfield-name",
+            label: "nome",
+            name: "name",
+            variant: "outlined",
+            onChange: handleChange,
+            value: formData.name,
+          },
+          {
+            id: "textfield-description",
+            label: "descrizione",
+            variant: "outlined",
+            name: "description",
+            onChange: handleChange,
+            value: formData.description,
+          },
+          {
+            select: true,
+            setFormData: setFormData,
+            menuItems: DIFFICULTY_SELECT_MENU_ITEMS,
+          },
+          {
+            id: "textfield-lat",
+            label: "lat",
+            variant: "outlined",
+            name: "lat",
+            value: formData.lat,
+            onChange: handleChange,
+          },
+          {
+            id: "textfield-long",
+            label: "long",
+            variant: "outlined",
+            name: "long",
+            value: formData.long,
+            onChange: handleChange,
+          },
+        ]}
       />
       <Button variant="contained" endIcon={<PublishIcon />} type="submit">
         Salva Boulder
