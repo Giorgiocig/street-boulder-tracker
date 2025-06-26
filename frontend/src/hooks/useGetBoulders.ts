fbdb5e06c0c20d986256a74b62eaefe42f355e6e
Giorgiocig <giorgiocignitti@gmail.com>
2025-06-26 14:36:47 +0200

    Implement useGetBoulders hook (frontend)

---
+++ frontend/src/hooks/useGetBoulders.ts
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBoulders } from "../api";
+
+export const useGetBoulders = () => {
+  return useQuery({
+    queryKey: ["boulders"],
+    queryFn: getBoulders,
+  });
+};
