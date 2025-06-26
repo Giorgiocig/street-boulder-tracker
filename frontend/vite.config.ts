689d9378527839dcd29cf64c1ec98b117a8d1a80
Giorgiocig <giorgiocignitti@gmail.com>
2025-06-26 10:44:40 +0200

    Update vite.config.ts to include test properties (frontend)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/vitest.setup.ts"],
+    css: true,
+    testTimeout: 5000,
+    reporters: ["verbose"],
+  },
+});
