a806ef3971595527cc1c54bf9985421d4edbc76b
Giorgiocig <giorgiocignitti@gmail.com>
2025-06-27 09:56:05 +0200

    Delete api and hooks folder (frontend)

--- frontend/src/hooks/useGetBoulders.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getBoulders } from "../api";
-
-export const useGetBoulders = () => {
-  return useQuery({
-    queryKey: ["boulders"],
-    queryFn: getBoulders,
-  });
-};
