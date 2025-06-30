b6bd48ccbb1744e57534e006f5f4f37e2bb68334
Giorgiocig <giorgiocignitti@gmail.com>
2025-06-30 12:12:44 +0200

    Add Main.tsx component (frontend)

---
+++ frontend/src/components/layout/Main.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Button } from "@mui/material";
+import BoulderForm from "./BoulderForm";
+
+export default function Main() {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Button variant="contained" size="large" onClick={() => setOpen(!open)}>
+        Inserisici boulder
+      </Button>
+      {open && <BoulderForm />}
+    </>
+  );
+}
