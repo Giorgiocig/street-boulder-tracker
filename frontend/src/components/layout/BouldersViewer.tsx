2b035bc3507c07006c8b5f3ae9f3dee6ee73f2db
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-02 10:14:38 +0200

    Decalare a sate to latLng in BouldersViewer and pass it to child component (frontned)

--- frontend/src/components/layout/BouldersViewer.tsx
+++ frontend/src/components/layout/BouldersViewer.tsx
@@ -2,15 +2,20 @@ import { Box } from "@mui/material";
 import { useGetBoulders } from "../../services";
 import BoulderCardViewer from "../common/BoulderCardViewer";
 import LeafletBouldersViewer from "../common/LeafletBouldersViewer";
+import { useState } from "react";
 
 export default function BouldersViewer() {
-  const response = useGetBoulders();
+  const [latLng, setLatLng] = useState<
+    [latitude: number, longitude: number] | null
+  >(null);
 
+  const response = useGetBoulders();
   const boulders = response.data;
+
   return (
-    <Box sx={{ display: "flex" }}>
-      <LeafletBouldersViewer boulders={boulders} />
-      <BoulderCardViewer boulders={boulders} />
+    <Box sx={{ display: "flex", gap: 4 }}>
+      <LeafletBouldersViewer boulders={boulders} latLng={latLng} />
+      <BoulderCardViewer boulders={boulders} setLatLng={setLatLng} />
     </Box>
   );
 }
