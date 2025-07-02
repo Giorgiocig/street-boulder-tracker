909c495da2e849edc0715e2d5e0e273e26c8f236
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-02 10:10:27 +0200

    Add functionality to localize boulder and to delete a boulder (frontend)

--- frontend/src/components/common/BasicCard.tsx
+++ frontend/src/components/common/BasicCard.tsx
@@ -3,26 +3,68 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import type { IBoulder } from "../../utilities";
 import CardActions from "@mui/material/CardActions";
-import { Button } from "@mui/material";
+import { Box, Button, IconButton, Stack } from "@mui/material";
 import MyLocationIcon from "@mui/icons-material/MyLocation";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { useDeleteBoulder } from "../../services";
 
-export default function BasicCard({ boulder }: { boulder: IBoulder }) {
+export default function BasicCard({
+  boulder,
+  setLatLng,
+}: {
+  boulder: IBoulder;
+  setLatLng: (arg: number[]) => void;
+}) {
+  const deleteBoulderMutation = useDeleteBoulder();
+
   const handleClickLocation = () => {
-    console.log("localize");
+    setLatLng([boulder.latitude, boulder.longitude]);
+  };
+
+  const handleClickDelete = async () => {
+    if (typeof boulder.id === "number")
+      await deleteBoulderMutation.mutateAsync(boulder.id);
+    else {
+      console.error("ID del boulder non valido:", boulder.id);
+    }
+  };
+
+  const handleClicEdit = () => {
+    console.log("Edit");
   };
   return (
     <Card>
-      <CardContent>
-        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
-          {boulder.description}
-        </Typography>
-        <Typography variant="h5" component="div">
-          {boulder.name}
-        </Typography>
-        <Typography sx={{ color: "text.secondary", mt: 1, mb: 1.5 }}>
-          {boulder.difficulty}
-        </Typography>
-      </CardContent>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <CardContent>
+          <Typography
+            gutterBottom
+            sx={{ color: "text.secondary", fontSize: 14 }}
+          >
+            {boulder.description}
+          </Typography>
+          <Typography variant="h5" component="div">
+            {boulder.name}
+          </Typography>
+          <Typography sx={{ color: "text.secondary", mt: 1, mb: 1.5 }}>
+            {boulder.difficulty}
+          </Typography>
+        </CardContent>
+        <Stack sx={{ gap: 2 }}>
+          <IconButton aria-label="delete" onClick={handleClickDelete}>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton aria-label="delete" onClick={handleClicEdit}>
+            <EditIcon />
+          </IconButton>
+        </Stack>
+      </Box>
       <CardActions>
         <Button
           variant="outlined"
