a80fd7b5222ab84e2f38b74b7f527cbc73f21350
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-02 12:05:30 +0200

    Embeed FullScreenDialog in Main (frontend)

--- frontend/src/components/layout/Main.tsx
+++ frontend/src/components/layout/Main.tsx
@@ -2,15 +2,24 @@ import { useState } from "react";
 import { Button } from "@mui/material";
 import BoulderForm from "./BoulderForm";
 import BouldersViewer from "./BouldersViewer";
+import FullScreenDialog from "../common/FullScreenDialog";
 
 export default function Main() {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <Button variant="contained" size="large" onClick={() => setOpen(!open)}>
+      <Button variant="contained" size="large" onClick={handleClickOpen}>
         Inserisici boulder
       </Button>
-      {open && <BoulderForm />}
+      <FullScreenDialog setIsOpen={setIsOpen} isOpen={isOpen}>
+        <BoulderForm />
+      </FullScreenDialog>
+
       <BouldersViewer />
     </>
   );
