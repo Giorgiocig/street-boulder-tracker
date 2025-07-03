af8a9abdb27c59ee9c2d47c898e6763138d547e3
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-03 10:19:46 +0200

    Add fixtures folder for test (frontend)

---
+++ frontend/src/components/layout/__test__/fixtures/fixtures.ts
@@ -0,0 +1,9 @@
+export const boulderFixture = {
+  id: 123,
+  latitude: 41.9,
+  longitude: 12.5,
+  name: "Boulder 1",
+  description: "Descrizione 1",
+  difficulty: "facile" as const,
+  createdAt: "2023-01-01T00:00:00.000Z",
+};
