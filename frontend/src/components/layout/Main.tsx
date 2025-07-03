3bf3a16ef6b533a2eec33fafcf85a3d81d604df0
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-03 12:09:09 +0200

    Add width e margin 0 auto to Box in Main component  (frontend)

--- frontend/src/components/layout/Main.tsx
+++ frontend/src/components/layout/Main.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import BoulderForm from "./BoulderForm";
 import BouldersViewer from "./BouldersViewer";
 import FullScreenDialog from "../common/FullScreenDialog";
@@ -13,14 +13,16 @@ export default function Main() {
 
   return (
     <>
-      <Button variant="contained" size="large" onClick={handleClickOpen}>
-        Inserisici boulder
-      </Button>
-      <FullScreenDialog setIsOpen={setIsOpen} isOpen={isOpen}>
-        <BoulderForm />
-      </FullScreenDialog>
+      <Box sx={{ "max-width": "1280px", margin: " 0 auto" }}>
+        <Button variant="contained" size="large" onClick={handleClickOpen}>
+          Inserisici boulder
+        </Button>
+        <FullScreenDialog setIsOpen={setIsOpen} isOpen={isOpen}>
+          <BoulderForm />
+        </FullScreenDialog>
 
-      <BouldersViewer />
+        <BouldersViewer />
+      </Box>
     </>
   );
 }
