88649430de567b54d206efc55c06394872fa5693
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-04 12:19:26 +0200

    Add type to difficulty to fix ts bug (frontend) in SelectForm

--- frontend/src/components/form/SelectForm.tsx
+++ frontend/src/components/form/SelectForm.tsx
@@ -1,28 +1,31 @@
 import {
   FormControl,
+  InputLabel,
   MenuItem,
   Select,
   type SelectChangeEvent,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import type { ISelectForm } from "../../utilities/interfaces";
+import type { Difficulty } from "../../utilities";
 
 export default function SelectForm({
   menuItems = [],
   setFormData,
   value,
 }: ISelectForm) {
-  const [difficulty, setDifficulty] = useState(value || "facile");
+  const [difficulty, setDifficulty] = useState<Difficulty>(value as Difficulty);
 
   useEffect(() => {
     setFormData((prev) => ({ ...prev, difficulty }));
   }, [difficulty]);
 
   const handleChangeSelect = (event: SelectChangeEvent) => {
-    setDifficulty(event.target.value as string);
+    setDifficulty(event.target.value as Difficulty);
   };
   return (
     <FormControl>
+      <InputLabel id="select-difficulty-label">Difficoltà</InputLabel>
       <Select
         labelId="select-difficulty"
         id="select-difficulty"
@@ -30,8 +33,10 @@ export default function SelectForm({
         label="Difficulty"
         onChange={handleChangeSelect}
       >
-        {menuItems.map((menuItem) => (
-          <MenuItem value={menuItem}>{menuItem}</MenuItem>
+        {menuItems.map((menuItem: string) => (
+          <MenuItem key={menuItem} value={menuItem}>
+            {menuItem}
+          </MenuItem>
         ))}
       </Select>
     </FormControl>
