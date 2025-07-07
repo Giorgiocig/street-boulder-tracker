0700c72cecbc502a7ec2d33af2023615b9903309
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-07 12:10:45 +0200

    Add ficture for event (backend)

--- backend/test/fixture/app.e2eFixture.ts
+++ backend/test/fixture/app.e2eFixture.ts
@@ -1,4 +1,6 @@
+import { Boulder } from '@prisma/client';
 import { BoulderDto } from 'src/boulder/dto';
+import { EventDto } from 'src/event/dto';
 import { Difficulty } from 'src/utilities/enums/enums';
 
 export * from './app.e2eFixture';
@@ -11,4 +13,14 @@ export const boulderDtoFixture: BoulderDto = {
   longitude: 7.123,
   createdAt: '2025-06-18T14:30:00.000Z',
   eventId: 1,
+};
+
+export const eventBodyRequestFixture: EventDto = {
+  name: 'evento',
+  description: 'description',
+  date: '2025-06-18T14:30:00.000Z',
+  city: 'Roma',
+  latitude: 1,
+  longitude: 2,
+  createdAt: '2025-06-18T14:30:00.000Z',
 };
