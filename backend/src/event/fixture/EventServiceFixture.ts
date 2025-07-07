4cfe0e6a7b076411cd6285ff764749d93e61ac5b
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-07 11:46:06 +0200

    Add eventFixture for unit test (backend)

---
+++ backend/src/event/fixture/EventServiceFixture.ts
@@ -0,0 +1,9 @@
+export const eventFixture = {
+  name: 'Event',
+  description: 'description',
+  date: '2025-06-18T14:30:00.000Z',
+  city: 'Milan',
+  latitude: 45.123,
+  longitude: 7.123,
+  createdAt: '2025-06-18T14:30:00.000Z',
+};
