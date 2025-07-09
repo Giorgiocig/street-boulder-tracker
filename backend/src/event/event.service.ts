40080ab4ebbe50f4f34f388e5523342ad7dde185
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-09 11:30:12 +0200

    Add service for getEvents, updateEvent, deleteEvent (backend)

--- backend/src/event/event.service.ts
+++ backend/src/event/event.service.ts
@@ -1,6 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { EventDto } from './dto';
+import { UpdateEventDto } from './dto/updateEvent.dto';
 
 @Injectable()
 export class EventService {
@@ -20,7 +27,63 @@ export class EventService {
       return await this.prisma.event.create({ data: event });
     } catch (error) {
       this.logger.error('Failed to create event', error.stack);
-      throw error;
+      throw new InternalServerErrorException('Failed to create event');
+    }
+  }
+  async getEvents() {
+    try {
+      return await this.prisma.event.findMany();
+    } catch (error) {
+      this.logger.error('Failed to fetch event', error.stack);
+      throw new Error('Failed to fetch events');
+    }
+  }
+
+  async updateEvent(id: number, dto: UpdateEventDto) {
+    try {
+      const existingEvent = await this.prisma.event.findUnique({
+        where: { id },
+      });
+
+      if (!existingEvent) {
+        this.logger.warn(`Event with ID ${id} not found`);
+        throw new NotFoundException(`Event with ID ${id} not found`);
+      }
+
+      const updatedEvent = {};
+      for (const [key, value] of Object.entries(dto)) {
+        if (value !== undefined) updatedEvent[key] = value;
+      }
+
+      return await this.prisma.event.update({
+        where: { id },
+        data: updatedEvent,
+      });
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      this.logger.error(`Failed to update event with ID ${id}`, error.stack);
+      throw new InternalServerErrorException('Failed to update event');
+    }
+  }
+
+  async deleteEvent(id: number) {
+    try {
+      const existingEvent = await this.prisma.event.findUnique({
+        where: { id },
+      });
+      if (!existingEvent) {
+        this.logger.warn(`Event with ID ${id} not found`);
+        throw new NotFoundException(`Event with ID ${id} not found`);
+      }
+      return await this.prisma.event.delete({ where: { id } });
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      this.logger.error(`Failed to delete event with ID ${id}`, error.stack);
+      throw new InternalServerErrorException('Failed to delete event', error);
     }
   }
 }
