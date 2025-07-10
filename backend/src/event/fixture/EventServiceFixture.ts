a31b6fa5638b999595c5939fce9bda8efe1eb295
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-10 11:52:11 +0200

    Add fixture for event with boulders (backend)

--- backend/src/event/fixture/EventServiceFixture.ts
+++ backend/src/event/fixture/EventServiceFixture.ts
@@ -1,3 +1,5 @@
+import { Difficulty } from 'src/utilities/enums/enums';
+
 export const eventFixture = {
   name: 'Test Event',
   description: 'description',
@@ -6,4 +8,35 @@ export const eventFixture = {
   latitude: 45.123,
   longitude: 7.123,
   createdAt: '2025-06-18T14:30:00.000Z',
+};
+
+export const eventWithBouldersFixture = {
+  id: 1,
+  name: 'Test Event',
+  description: 'description',
+  date: '2025-06-18T14:30:00.000Z',
+  city: 'Milan',
+  latitude: 45.123,
+  longitude: 7.123,
+  createdAt: '2025-06-18T14:30:00.000Z',
+  boulders: [
+    {
+      name: 'Test Boulder1',
+      description: 'Nice boulder',
+      difficulty: Difficulty.facile,
+      latitude: 45.123,
+      longitude: 7.123,
+      createdAt: '2025-06-18T14:30:00.000Z',
+      eventId: 1,
+    },
+    {
+      name: 'Test Boulder2',
+      description: 'Nice boulder',
+      difficulty: Difficulty.facile,
+      latitude: 45.123,
+      longitude: 7.123,
+      createdAt: '2025-06-18T14:30:00.000Z',
+      eventId: 1,
+    },
+  ],
 };
