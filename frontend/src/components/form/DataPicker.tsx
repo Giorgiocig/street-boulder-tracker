1a555cc4ca06ca798f83beea0e7f6d12821f290e
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-21 10:11:03 +0200

    Change color to DataPicker icon (frontend)

--- frontend/src/components/form/DataPicker.tsx
+++ frontend/src/components/form/DataPicker.tsx
@@ -3,8 +3,6 @@ import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
-import { IconButton, InputAdornment } from "@mui/material";
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 
 export default function ResponsiveDatePickers({
   onSelect,
@@ -26,15 +24,9 @@ export default function ResponsiveDatePickers({
             onChange={onSelect}
             defaultValue={dayjs("2022-04-17")}
             slotProps={{
-              textField: {
-                InputProps: {
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton>
-                        <CalendarTodayIcon sx={{ color: "#2E8B57" }} />
-                      </IconButton>
-                    </InputAdornment>
-                  ),
+              openPickerButton: {
+                sx: {
+                  color: "#2E8B57",
                 },
               },
             }}
