35e69522aa93b78fb4322a4e7dee880e9b7b5fc6
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-24 11:39:03 +0200

    Implement updateEvent and deleteEvent service functions (frontend)

--- frontend/src/services/api/Event/eventApi.ts
+++ frontend/src/services/api/Event/eventApi.ts
@@ -1,3 +1,5 @@
+import type { IEventForm } from "../../../utilities";
+
 export const addEvent = async (data: any): Promise<any> => {
   const res = await fetch("http://localhost:3000/v1/events/add", {
     method: "POST",
@@ -7,8 +9,27 @@ export const addEvent = async (data: any): Promise<any> => {
   return res.json();
 };
 
-export const getEvents = async () => {
+export const getEvents = async (): Promise<any> => {
   const res = await fetch("http://localhost:3000/v1/events/get");
   const data = await res.json();
   return data;
+};
+
+export const deleteEvent = async (id: number): Promise<any> => {
+  const res = await fetch(`http://localhost:3000/v1/events/${id}`, {
+    method: "DELETE",
+  });
+  return res.json();
+};
+
+export const updateEvent = async (
+  id: number,
+  data: IEventForm
+): Promise<any> => {
+  const res = await fetch(`http://localhost:3000/v1/events/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return res.json;
 };
