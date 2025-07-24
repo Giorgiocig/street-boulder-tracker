8e63fe3c4fe6b7a8208093b4486d268e77b8d71c
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-24 11:41:19 +0200

    Refactor prop in EventCardViewer (frontend)

--- frontend/src/components/common/EventCardViewer.tsx
+++ frontend/src/components/common/EventCardViewer.tsx
@@ -1,22 +1,17 @@
 import { Box } from "@mui/material";
 import { useGetEvents } from "../../services";
 import EventCard from "./EventCard";
-import type { IEventCard } from "../../utilities";
+import type { IEventCard, IEventForm } from "../../utilities";
 
 export default function EventCardViewer() {
   const response = useGetEvents();
   const { data } = response;
+
   return (
     <Box>
       {data &&
         data.map((event: IEventCard) => (
-          <EventCard
-            key={event.id}
-            name={event.name}
-            description={event.description}
-            date={event.date}
-            city={event.city}
-          />
+          <EventCard key={event.id} {...(event as IEventForm)} />
         ))}
     </Box>
   );
