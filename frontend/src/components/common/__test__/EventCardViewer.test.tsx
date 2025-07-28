39efb08f278cb9a60730a00a427e1a0ca73d9530
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-28 11:30:01 +0200

    Add unit test for EventCardViewer (frontend)

---
+++ frontend/src/components/common/__test__/EventCardViewer.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, vi, expect } from "vitest";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import EventCardViewer from "../EventCardViewer";
+
+const queryClient = new QueryClient();
+
+vi.mock("../../../services", () => ({
+  useGetEvents: () => ({
+    data: [
+      {
+        id: 1,
+        name: "Evento 1",
+        description: "event test",
+        date: "2023-01-01T00:00:00.000Z",
+        city: "roma",
+        latitude: 1,
+        longitude: 2,
+        createdAt: "2023-01-01T00:00:00.000Z",
+      },
+    ],
+    isLoading: false,
+  }),
+  useDeleteEvent: () => ({
+    mutateAsync: vi.fn(),
+  }),
+}));
+
+describe("EventCardViewer", () => {
+  it("should render Card", () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <EventCardViewer />
+      </QueryClientProvider>
+    );
+    expect(screen.getByText("Evento 1")).toBeInTheDocument();
+  });
+});
