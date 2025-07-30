a589a5233d948a90ef1021f2b18d81487803b4e3
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-30 12:08:52 +0200

    Add routes to App.tsx (frontend)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,25 @@
 import "leaflet/dist/leaflet.css";
-import Main from "./components/layout/Main";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./theme";
 import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BoulderEditor from "./components/layout/BoulderEditor";
+import Layout from "./components/layout/Layout";
+import EventEditor from "./components/common/EventEditor";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Main />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<EventEditor />} />
+              <Route path="event/:eventId" element={<BoulderEditor />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </>
   );
