25c2d5ef9a292274fb7c4ad862553b876ac31ed1
Giorgiocig <giorgiocignitti@gmail.com>
2025-07-30 12:14:25 +0200

    Add useNavigate and handleClick to redirect in EventCardViewer (frontend)

--- frontend/src/components/common/EventCardViewer.tsx
+++ frontend/src/components/common/EventCardViewer.tsx
@@ -2,16 +2,26 @@ import { Box } from "@mui/material";
 import { useGetEvents } from "../../services";
 import EventCard from "./EventCard";
 import type { IEventCard, IEventForm } from "../../utilities";
+import { useNavigate } from "react-router";
 
 export default function EventCardViewer() {
   const response = useGetEvents();
   const { data } = response;
+  const navigate = useNavigate();
+
+  const handleClickEvent = (eventId: number) => {
+    navigate(`/event/${eventId}`);
+  };
 
   return (
     <Box>
       {data &&
         data.map((event: IEventCard) => (
-          <EventCard key={event.id} {...(event as IEventForm)} />
+          <EventCard
+            key={event.id}
+            {...(event as IEventForm)}
+            handleClickEvent={handleClickEvent}
+          />
         ))}
     </Box>
   );
