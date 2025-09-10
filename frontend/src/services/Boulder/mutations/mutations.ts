dcf658e0a89b85a3ec3ad63b25cd9957b93a057d
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-10 15:49:16 +0200

    Add onSucess and onError to mutations for Boulder (mutations-frontend)

--- frontend/src/services/Boulder/mutations/mutations.ts
+++ frontend/src/services/Boulder/mutations/mutations.ts
@@ -1,8 +1,10 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { addBoulder, deleteBoulder, updateBoulder } from "../boulderApi";
 import type { IBoulder } from "../../../utilities/interfaces";
+import { useSnackbar } from "../../../contexts/SnackbarContext";
 
 export const useAddBoulder = () => {
+  const { showSnackbar } = useSnackbar();
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (data: IBoulder) => addBoulder(data),
@@ -14,9 +16,11 @@ export const useAddBoulder = () => {
     },
     onSuccess: () => {
       console.log("success");
+      showSnackbar("Buolder creato con successo");
     },
     onError: () => {
       console.log("error");
+      showSnackbar("Errore nella creazione del boulder");
     },
     // on Settled --> end  mutation
     onSettled: async (_, error) => {
@@ -30,9 +34,18 @@ export const useAddBoulder = () => {
 };
 
 export const useDeleteBoulder = () => {
+  const { showSnackbar } = useSnackbar();
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (id: number) => deleteBoulder(id),
+    onSuccess: () => {
+      console.log("success");
+      showSnackbar("Buolder eliminato con successo");
+    },
+    onError: () => {
+      console.log("error");
+      showSnackbar("Errore nell eliminazione del boulder");
+    },
     onSettled: async (_, error) => {
       if (error) console.log(error);
       else await queryClient.invalidateQueries({ queryKey: ["events"] });
@@ -41,10 +54,19 @@ export const useDeleteBoulder = () => {
 };
 
 export const useUpdateBoulder = () => {
+  const { showSnackbar } = useSnackbar();
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: ({ id, data }: { id: number; data: IBoulder }) =>
       updateBoulder(id, data),
+    onSuccess: () => {
+      console.log("success");
+      showSnackbar("Buolder aggiornato con successo");
+    },
+    onError: () => {
+      console.log("error");
+      showSnackbar("Errore nell aggiornamento del boulder");
+    },
     onSettled: async (_, error) => {
       if (error) console.log(error);
       else
