45b312f50d79e9120ba59b3f8cfd8055041747dd
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-12 13:58:34 +0200

    Add LatLongProvider (main - frontend)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SnackbarProvider } from "./contexts/SnackbarContext.tsx";
+import { LatLongProvider } from "./contexts/LatLongContext.tsx";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <SnackbarProvider>
-        <App />
+        <LatLongProvider>
+          <App />
+        </LatLongProvider>
       </SnackbarProvider>
     </QueryClientProvider>
   </StrictMode>
