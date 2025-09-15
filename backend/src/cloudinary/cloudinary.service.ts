ad962d6fc4c32009f20b70ba498c895e1119781d
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-15 10:40:48 +0200

    Add cloudinary service and register env variables (cloudinary - backend)

---
+++ backend/src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { v2 as cloudinary } from 'cloudinary';
+
+@Injectable()
+export class CloudinaryService {
+  constructor(private readonly config: ConfigService) {
+    cloudinary.config({
+      cloud_name: this.config.get<string>('CLOUDINARY_CLOUD_NAME'),
+      api_key: this.config.get<string>('CLOUDINARY_API_KEY'),
+      api_secret: this.config.get<string>('CLOUDINARY_API_SECRET'),
+    });
+  }
+  async uploadImage() {
+    try {
+    } catch (error) {}
+  }
+}
