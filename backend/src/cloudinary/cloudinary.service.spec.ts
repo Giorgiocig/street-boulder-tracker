f44d8e5d916521c50387dccffdcb12f546673118
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-16 12:10:43 +0200

    Refactor cloudinary.service test (cloudinary.service.test - backend)

--- backend/src/cloudinary/cloudinary.service.spec.ts
+++ backend/src/cloudinary/cloudinary.service.spec.ts
@@ -4,6 +4,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
 import { createBaseDto } from 'src/boulder/fixture';
 import { v2 as cloudinary } from 'cloudinary';
+import { cloudinaryConfigTest, imageResultFixture } from './test.utilities';
 
 describe('CloudinaryService', () => {
   let service: CloudinaryService;
@@ -21,12 +22,7 @@ describe('CloudinaryService', () => {
 
   const mockConfigService = {
     get: jest.fn((key: string) => {
-      const config = {
-        CLOUDINARY_CLOUD_NAME: 'test-cloud',
-        CLOUDINARY_API_KEY: 'test-api-key',
-        CLOUDINARY_API_SECRET: 'test-api-secret',
-      };
-      return config[key];
+      return cloudinaryConfigTest[key];
     }),
   };
 
@@ -63,35 +59,29 @@ describe('CloudinaryService', () => {
 
   describe('uploadImage', () => {
     it('should upload an image successfully', async () => {
-      // Mock boulder trovato
+      // Mock found boulder
       const mockBoulder = createBaseDto();
       mockPrismaService.boulder.findUnique.mockResolvedValue(mockBoulder);
 
-      // Mock risultato immagine creata
-      const mockImageResult = {
-        id: 3,
-        url: 'https://res.cloudinary.com/ddcyifrfx/image/upload/v1758010208/boulders/iyt2iary8hgj3jrmdpg4.jpg',
-        public_id: 'boulders/iyt2iary8hgj3jrmdpg4',
-        uploadedAt: '2025-09-16T08:10:09.126Z',
-        boulderId: 1,
-      };
-      mockPrismaService.image.create.mockResolvedValue(mockImageResult);
+      // Mock found image
+
+      mockPrismaService.image.create.mockResolvedValue(imageResultFixture);
 
       // Mock Cloudinary upload_stream
       const mockStream = {
         end: jest.fn(),
       };
 
+      // Mimick an async call with setTimeout
       jest
         .spyOn(cloudinary.uploader, 'upload_stream')
         .mockImplementation(
           (options: any, callback?: (error: any, result: any) => void) => {
-            // Simula una chiamata asincrona con setTimeout per essere più realistico
             setTimeout(() => {
               if (callback) {
                 callback(null, {
                   secure_url:
-                    'https://res.cloudinary.com/ddcyifrfx/image/upload/v1758010208/boulders/iyt2iary8hgj3jrmdpg4.jpg',
+                    'https://res.cloudinary.com/ddcyifrfx/image/upload/v1758014220/boulders/vxvqxoyfc1lpyuacjfhg.jpg',
                   public_id: 'boulders/iyt2iary8hgj3jrmdpg4',
                 });
               }
@@ -103,29 +93,27 @@ describe('CloudinaryService', () => {
       const fileBuffer = Buffer.from('mock image data');
       const result = await service.uploadImage(fileBuffer, 1);
 
-      // Verifiche
+      // Verify that boulder exists
       expect(mockPrismaService.boulder.findUnique).toHaveBeenCalledWith({
         where: { id: 1 },
       });
 
       expect(mockPrismaService.image.create).toHaveBeenCalledWith({
         data: {
-          url: 'https://res.cloudinary.com/ddcyifrfx/image/upload/v1758010208/boulders/iyt2iary8hgj3jrmdpg4.jpg',
+          url: 'https://res.cloudinary.com/ddcyifrfx/image/upload/v1758014220/boulders/vxvqxoyfc1lpyuacjfhg.jpg',
           public_id: 'boulders/iyt2iary8hgj3jrmdpg4',
           boulderId: 1,
         },
       });
 
-      expect(result).toEqual(mockImageResult);
+      expect(result).toEqual(imageResultFixture);
       expect(mockStream.end).toHaveBeenCalledWith(fileBuffer);
     });
 
     it('should throw error when boulder not found', async () => {
-      // Mock boulder non trovato
+      // Mock not found boulder
       mockPrismaService.boulder.findUnique.mockResolvedValue(null);
-
       const fileBuffer = Buffer.from('mock image data');
-
       await expect(service.uploadImage(fileBuffer, 999)).rejects.toThrow(
         'Failed to create boulder',
       );
@@ -133,16 +121,13 @@ describe('CloudinaryService', () => {
       expect(mockPrismaService.boulder.findUnique).toHaveBeenCalledWith({
         where: { id: 999 },
       });
-
-      // Verifica che console.error sia stato chiamato
-      expect(consoleErrorSpy).toHaveBeenCalled();
     });
 
     it('should handle cloudinary upload error', async () => {
-      // Mock boulder trovato
+      // Mock found boulder
       mockPrismaService.boulder.findUnique.mockResolvedValue(createBaseDto());
 
-      // Mock Cloudinary upload_stream con errore
+      // Mock Cloudinary upload_stream with error
       const mockStream = {
         end: jest.fn(),
       };
@@ -165,8 +150,6 @@ describe('CloudinaryService', () => {
       await expect(service.uploadImage(fileBuffer, 1)).rejects.toThrow(
         'Failed to create boulder',
       );
-
-      expect(consoleErrorSpy).toHaveBeenCalled();
     });
   });
 });
