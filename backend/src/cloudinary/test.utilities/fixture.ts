18c208c1931e3d551d6bdbea8c1cacd2801dd12f
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-16 12:10:15 +0200

    Add fixture imageResultFixture (test.utilities - backend)

---
+++ backend/src/cloudinary/test.utilities/fixture.ts
@@ -0,0 +1,7 @@
+export const imageResultFixture = {
+  id: 3,
+  url: 'https://res.cloudinary.com/ddcyifrfx/image/upload/v1758014220/boulders/vxvqxoyfc1lpyuacjfhg.jpg',
+  public_id: 'boulders/iyt2iary8hgj3jrmdpg4',
+  uploadedAt: '2025-09-16T08:10:09.126Z',
+  boulderId: 1,
+};
