8b4f9ca61ae055573a977856b6736fa3fd3edc94
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-17 11:48:15 +0200

    Add error handling to cloudinary services (cloudinary.service - backend)

--- backend/src/cloudinary/cloudinary.service.ts
+++ backend/src/cloudinary/cloudinary.service.ts
@@ -26,11 +26,10 @@ export class CloudinaryService {
       const boulder = await this.prisma.boulder.findUnique({
         where: { id: boulderId },
       });
-
       if (!boulder) {
-        throw new Error(`Boulder with id ${boulderId} not found`);
+        throw new NotFoundException(`Boulder with id ${boulderId} not found`);
       }
-      // Promise is required because Cloudinary API doesn t return a promise.
+      // Upload image on Cloudinary
       const result = await new Promise<any>((resolve, reject) => {
         const stream = cloudinary.uploader.upload_stream(
           { folder: 'boulders' },
@@ -42,18 +41,20 @@ export class CloudinaryService {
         stream.end(fileBuffer);
       });
 
-      const dto: ImageDto = {
-        url: result.secure_url,
-        public_id: result.public_id,
-        boulderId,
-      };
-
       return await this.prisma.image.create({
-        data: dto,
+        data: {
+          url: result.secure_url,
+          public_id: result.public_id,
+          boulderId,
+        },
       });
     } catch (error) {
-      console.error(error);
-      throw new Error('Failed to upload image', error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        `Failed to upload image: ${error.message}`,
+      );
     }
   }
   async getImages(boulderId: number) {
