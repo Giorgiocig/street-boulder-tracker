fe358763517918411a551f0b223b9bb23b578864
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-19 14:34:36 +0200

    Update BoulderSchema in zod with difficulty (scehma - frontend)

--- frontend/src/zodSchemas/BoulderSchema.ts
+++ frontend/src/zodSchemas/BoulderSchema.ts
@@ -1,5 +1,11 @@
 import * as z from "zod";
 
+import { DIFFICULTY_SELECT_MENU_ITEMS } from "../utilities";
+
 export const BoulderSchema = z.object({
   name: z.string().min(1, "Il nome del boulder è obbligatorio."),
+  description: z.string().min(1, "la descrizione è obbligatoria."),
+  difficulty: z.enum(DIFFICULTY_SELECT_MENU_ITEMS, {
+    error: "La difficoltà è obbligatoria.",
+  }),
 });
