430e580e13a55322d4a50a1503037d7b57c99946
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-23 12:22:49 +0200

    Add functionality for geolocation in BoulderFormRHF and fix data obj to be sended (BoulderFormRHF-frontend)

--- frontend/src/components/layout/BoulderFormRHF.tsx
+++ frontend/src/components/layout/BoulderFormRHF.tsx
@@ -1,27 +1,33 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { FormProvider, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { BoulderSchema } from "../../zodSchemas/BoulderSchema";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 import PublishIcon from "@mui/icons-material/Publish";
 import MyLocationIcon from "@mui/icons-material/MyLocation";
 import { useAddBoulder } from "../../services";
 import SelectForm from "../form/SelectForm";
-import { DIFFICULTY_SELECT_MENU_ITEMS, type IBoulder } from "../../utilities";
+import {
+  DIFFICULTY_SELECT_MENU_ITEMS,
+  type Difficulty,
+  type IBoulder,
+} from "../../utilities";
 import { useGeolocation } from "../../customHooks/useLocalization";
 import LeafletMapViewer from "../common/LeafletMapViewer";
 import { useEffect, useState } from "react";
 import { NumberInputRHF } from "../form/NumberInputRHF";
+import { useParams } from "react-router";
 export type BoulderSchemaValues = z.infer<typeof BoulderSchema>;
 
-export default function BoulderFormRHF({ boulder }: { boulder?: IBoulder }) {
-  const [getLocalization, setGetLocalization] = useState(false);
+export default function BoulderFormRHF({ boulder }: { boulder?: any }) {
+  const { eventId } = useParams<{ eventId: string }>();
   const createBoulderMutation = useAddBoulder();
   // form setup
   const {
     register,
     handleSubmit,
     setValue,
+    getValues,
     reset,
     watch,
     control,
@@ -32,25 +38,38 @@ export default function BoulderFormRHF({ boulder }: { boulder?: IBoulder }) {
       name: "",
       description: "",
       difficulty: "facile",
-      latitude: 0,
-      longitude: 0,
+      latitude: 41.9028,
+      longitude: 12.4964,
     },
   });
 
   // GEOLOCATION
-  const { geolocation, errorGeolocation, loadingGeolocation } =
-    useGeolocation(getLocalization);
+  const {
+    geolocation,
+    errorGeolocation,
+    loadingGeolocation,
+    refreshGeolocation,
+  } = useGeolocation();
 
   const handleClickLocation = () => {
-    setGetLocalization(false);
-    setTimeout(() => {
-      setGetLocalization(true);
-    }, 0);
+    refreshGeolocation();
   };
 
+  useEffect(() => {
+    if (geolocation) {
+      setValue("latitude", geolocation.latitude);
+      setValue("longitude", geolocation.longitude);
+    }
+  }, [geolocation]);
+
+  // submit
   const onSubmit = (data: BoulderSchemaValues) => {
-    //createBoulderMutation.mutate(data);
-    console.log(data);
+    createBoulderMutation.mutate({
+      ...data,
+      difficulty: data.difficulty as Difficulty,
+      eventId: parseFloat(eventId!),
+      createdAt: new Date().toISOString(),
+    });
   };
 
   return (
@@ -111,11 +130,11 @@ export default function BoulderFormRHF({ boulder }: { boulder?: IBoulder }) {
       >
         Localizzati
       </Button>
-      {/* <LeafletMapViewer
-        latLong={boulder ? [latLng.latitude, latLng.longitude] : latLng}
-        setFormData={setLatLng}
+      <LeafletMapViewer
+        latLong={[watch("latitude"), watch("longitude")]}
+        setValue={setValue}
         name={watch("name")}
-      /> */}
+      />
       <Button
         variant="contained"
         endIcon={<PublishIcon />}
@@ -130,6 +149,9 @@ export default function BoulderFormRHF({ boulder }: { boulder?: IBoulder }) {
       >
         Inserisci Boulder
       </Button>
+      {errorGeolocation && (
+        <Typography>Impossibile ottenere la localizzazione</Typography>
+      )}
     </form>
   );
 }
