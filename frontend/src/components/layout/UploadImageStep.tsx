a236715bded756446709fbb4f40631619faaad50
Giorgiocig <giorgiocignitti@gmail.com>
2025-09-29 12:12:38 +0200

    Add style to UploadImageStep (UploadImageStep-frontend)

--- frontend/src/components/layout/UploadImageStep.tsx
+++ frontend/src/components/layout/UploadImageStep.tsx
@@ -5,9 +5,13 @@ import { Box } from "@mui/material";
 export default function UploadImageStep() {
   return (
     <>
-      <Box sx={{ display: "flex" }}>
-        <ImageDisplayerStep />
-        <ButtonImageUpload />
+      <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <Box>
+          <ImageDisplayerStep />
+        </Box>
+        <Box>
+          <ButtonImageUpload />
+        </Box>
       </Box>
     </>
   );
