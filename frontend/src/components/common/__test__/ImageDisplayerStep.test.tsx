f66cd95d1027357d4a07a2257633684ad601cd34
Giorgiocig <giorgiocignitti@gmail.com>
2025-10-28 14:39:06 +0100

    Implement test (ImageDisplayeStep - frontend)

--- frontend/src/components/common/__test__/ImageDisplayerStep.test.tsx
+++ frontend/src/components/common/__test__/ImageDisplayerStep.test.tsx
@@ -1,11 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { cleanup, logRoles, render, screen } from "@testing-library/react";
 import { describe, it, vi, expect } from "vitest";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ImageDisplayerStep from "../ImageDisplayerStep";
 import testImg from "./testImg/testimage.jpeg";
 import { BoulderIdProvider } from "../../../contexts";
+import { userEvent, type UserEvent } from "@testing-library/user-event";
+import { useDeleteBoulderImage } from "../../../services/BoulderImage";
 
 const queryClient = new QueryClient();
+const mockMutate = vi.fn();
 
 vi.mock("../../../services/BoulderImage/queries/queries", () => ({
   useGetBoulderImages: vi.fn(() => ({
@@ -23,16 +26,43 @@ vi.mock("../../../services/BoulderImage/queries/queries", () => ({
   })),
 }));
 
+vi.mock("../../../services/BoulderImage/mutations/mutations", () => ({
+  useDeleteBoulderImage: vi.fn(() => ({
+    mutate: mockMutate,
+  })),
+}));
+
 describe("ImageDisplayerStep", () => {
-  it("should display image", () => {
-    render(
+  let user: UserEvent;
+  beforeEach(() => {
+    user = userEvent.setup();
+    const { container } = render(
       <QueryClientProvider client={queryClient}>
         <BoulderIdProvider>
           <ImageDisplayerStep />
         </BoulderIdProvider>
       </QueryClientProvider>
     );
+    screen.debug();
+    logRoles(container);
+  });
+  afterEach(() => {
+    cleanup();
+  });
+  it("should display image", () => {
     const img = screen.getByRole("img");
     expect(img).toBeInTheDocument();
+  });
+  it("should render the delete btn", () => {
+    const handleDeleteBtn = screen.getAllByRole("button", {
+      name: "delete btn",
+    });
+    expect(handleDeleteBtn).toHaveLength(1);
+  });
+  it("should call mutate when delete button is clicked", async () => {
+    const deleteBtn = screen.getByRole("button", { name: "delete btn" });
+    await user.click(deleteBtn);
+    expect(mockMutate).toHaveBeenCalledTimes(1);
+    expect(mockMutate).toHaveBeenCalledWith("boulders%2Fs3sx7gantis1ncqhmunf");
   });
 });
