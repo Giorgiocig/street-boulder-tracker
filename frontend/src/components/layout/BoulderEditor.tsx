bfc43ee64096d92cd0f0146c2d2d03b411629bc5
Giorgiocig <giorgiocignitti@gmail.com>
2025-10-30 11:52:22 +0100

    Embeed BasicBreadcrumbs in BoulderEditor (BoulderEditor-frontend)

--- frontend/src/components/layout/BoulderEditor.tsx
+++ frontend/src/components/layout/BoulderEditor.tsx
@@ -4,11 +4,13 @@ import FullScreenDialog from "../common/FullScreenDialog";
 import { useToggle } from "../../customHooks/useToggle";
 import PositionedSnackbar from "../common/Snackbar";
 import LinearStepper from "../common/LinearStepper";
+import BasicBreadcrumbs from "../common/BasicBreadcrumbs";
 
 export default function BoulderEditor() {
   const [value, setToggle] = useToggle(false);
   return (
     <Box sx={{ "max-width": "1280px", margin: " 0 auto", minHeight: "84.3vh" }}>
+      <BasicBreadcrumbs />
       <Button variant="contained" size="large" onClick={setToggle}>
         Inserisci Boulder
       </Button>
